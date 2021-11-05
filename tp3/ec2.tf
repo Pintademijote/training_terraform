6e1e5b411a5a1ff09c229df09ec505a68d67b45f
Pintademijote <pg.lemasle@gmail.com>
2021-11-05 16:07:07 +0100

    add prenom

--- tp3/ec2.tf
+++ tp3/ec2.tf
@@ -42,7 +42,7 @@ data "aws_ami" "amazon-linux-2" {
    instance_type = "t2.micro"
 
    tags = {
-       Name = "${var.instance_name }"
+       Name = "${var.prenom}"
        Formation = "ajc"
    }
 
