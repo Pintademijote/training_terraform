b479239db81305a4d12beac1e5f46a5d36d71be2
Pintademijote <pg.lemasle@gmail.com>
2021-11-05 12:38:44 +0100

    add first tf to tp2

---
+++ tp2/ec2.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+ resource "aws_instance" "pg_centos7_t2micro" {
+   ami           = "ami-0083662ba17882949"
+   instance_type = "t2.micro"
+
+   tag = {
+       Name = "pg"
+       Formation = "ajc"
+   }
+ }
