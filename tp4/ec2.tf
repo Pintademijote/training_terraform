19bfdf9c9a6b19a74def4eaf5dd580d22480c3db
Pintademijote <pg.lemasle@gmail.com>
2021-11-08 15:33:44 +0100

    add egress

--- tp4/ec2.tf
+++ tp4/ec2.tf
@@ -89,6 +89,17 @@ data "aws_ami" "amazon-linux-2" {
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
     }
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
   }
 
 
